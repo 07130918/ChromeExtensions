4e3bb00d8ff2b4abc8be17dee688ee852c8ac5a5
こた <tokyosato1@gmail.com>
2022-02-03 16:22:12 +0900

    todoとnottodoのハードコーディング

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-  </div>
+    <div id="app">
+        <!-- keep-aliveしたい -->
+        <div class="to-do-list">
+            <h1>To Do List</h1>
+            <div v-for="(key, i) in isInputablesOfToDo.length" :key="key">
+                <input type="checkbox" v-model="isInputablesOfToDo[i]">
+                <input type="text" v-model="toDoList[i]" autocomplete="off" :disabled="isInputablesOfToDo[i]">
+            </div>
+            <input type="button" value="clear" @click="clearToDoValues">
+        </div>
+        <div class="not-to-do-list">
+            <h1>Not To Do List</h1>
+            <div v-for="(key, t) in isInputablesOfNotToDo.length" :key="key">
+                <input type="checkbox" v-model="isInputablesOfNotToDo[t]">
+                <input type="text" v-model="notToDoList[t]" autocomplete="off" :disabled="isInputablesOfNotToDo[t]">
+            </div>
+            <input type="button" value="clear" @click="clearNotToDoValues">
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    name: 'App'
+    name: 'App',
+    data() {
+        return {
+            toDoList: [],
+            notToDoList: [],
+            isInputablesOfToDo: [false, false, false, false, false, false],
+            isInputablesOfNotToDo: [false, false, false],
+        }
+    },
+    methods: {
+        clearToDoValues() {
+            this.toDoList = [];
+            this.isInputablesOfToDo = [false, false, false, false, false, false];
+        },
+        clearNotToDoValues() {
+            this.notToDoList = [];
+            this.isInputablesOfNotToDo = [false, false, false];
+        },
+    }
 }
 </script>
 
-<style>
+<style scoped>
+input {
+    display: inline-block;
+}
 </style>
