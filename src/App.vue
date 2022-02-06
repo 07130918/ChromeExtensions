7d13b38b9962b6b75890f6dbdf8f9a0bc9de08a0
こた <tokyosato1@gmail.com>
2022-02-06 10:37:33 +0900

    NOTの方もコンポーネントに

--- src/App.vue
+++ src/App.vue
@@ -2,37 +2,20 @@
     <div id="app">
         <!-- keep-aliveしたい -->
         <to-do-list></to-do-list>
-        <div class="not-to-do-list">
-            <h1>Not To Do List</h1>
-            <div v-for="(key, t) in isInputablesOfNotToDo.length" :key="key">
-                <input type="checkbox" v-model="isInputablesOfNotToDo[t]">
-                <input type="text" v-model="notToDoList[t]" autocomplete="off" :disabled="isInputablesOfNotToDo[t]">
-            </div>
-            <input type="button" value="clear" @click="clearNotToDoValues">
-        </div>
+        <not-to-do-list></not-to-do-list>
     </div>
 </template>
 
 <script>
 import ToDoList from './components/ToDoList.vue'
+import NotToDoList from './components/NotToDoList.vue'
 
 export default {
     name: 'App',
     components: {
         'to-do-list': ToDoList,
+        'not-to-do-list': NotToDoList,
     },
-    data() {
-        return {
-            notToDoList: [],
-            isInputablesOfNotToDo: [false, false, false],
-        }
-    },
-    methods: {
-        clearNotToDoValues() {
-            this.notToDoList = [];
-            this.isInputablesOfNotToDo = [false, false, false];
-        },
-    }
 }
 </script>
 
---
+++ src/components/NotToDoList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="not-to-do-list">
+        <h1>Not To Do List</h1>
+        <div v-for="list in notToDoList" :key="list.content">
+            <input type="checkbox" v-model="list.isInputable">
+            <input type="text" v-model="list.content" autocomplete="off" :disabled="list.isInputable">
+        </div>
+        <input type="button" value="Reset" @click="createList">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            notToDoList: [],
+            notToDoListObj: {
+                isInputable: false,
+                content: '',
+            },
+            listLength: 6,
+        }
+    },
+    created() {
+        this.createList();
+    },
+    methods: {
+        createList() {
+            if (this.notToDoList.length != 0) this.notToDoList = [];
+
+            for (let index = 0; index < this.listLength; index++) {
+                this.notToDoList.push({...this.notToDoListObj});
+            }
+        },
+    }
+}
+</script>
