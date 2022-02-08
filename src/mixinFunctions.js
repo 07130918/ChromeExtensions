0d7632f3a9423fa39d5c9abd73ef558e0929f516
こた <tokyosato1@gmail.com>
2022-02-08 11:38:54 +0900

    storage保存はmixinに移動

--- src/mixinFunctions.js
+++ src/mixinFunctions.js
@@ -1,11 +1,17 @@
 export default {
     methods: {
-        createList(targetListLength) {
+        createList(targetListLength, objectName) {
             const generatedList = [];
             for (let i = 0; i < targetListLength; i++) {
                 generatedList.push({ isInputable: false, content: '' });
             }
+            this.setToChromeStorage(objectName, generatedList);
             return generatedList;
+        },
+        setToChromeStorage(objectName, object) {
+            chrome.storage.local.set({ [objectName]: object }, function () {
+                console.log(`${objectName} saved`);
+            });
         },
     },
 }
