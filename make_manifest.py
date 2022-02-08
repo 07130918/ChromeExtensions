e8c0e0560bcfe838b070b760801246dc651a802d
こた <tokyosato1@gmail.com>
2022-02-08 12:56:11 +0900

    manifestを作るスクリプトの作成

---
+++ make_manifest.py
@@ -0,0 +1,26 @@
+# make manifest.json
+# type python make_manifest.py
+
+import os
+import json
+
+
+def main():
+    content = {
+        "name": "To Do List",
+        "description": "A simple To Do List extension",
+        "manifest_version": 3,
+        "version": "1.0",
+        "permissions": ["activeTab", "scripting", "storage"],
+        "action": {
+            "default_icon": "icon.png",
+            "default_popup": "index.html"
+        }
+    }
+    os.system('touch dist/manifest.json')
+    with open('dist/manifest.json', 'w') as f:
+        json.dump(content, f, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    main()
