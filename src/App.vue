74aad86bb06c483211bce52f9fe2989a58dfaa91
こた <tokyosato1@gmail.com>
2022-02-10 15:35:40 +0900

    darkMode 追加

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,50 @@
 <template>
     <div id="app">
-        <div id="list-title-wrapper">
-            <h5 :class="{'active-component': isToDo}" @click="componentChangeTo('to-do-list')">To Do List</h5>
-            <h5 :class="{'active-component': isNotToDo}" @click="componentChangeTo('not-to-do-list')">Not To Do List</h5>
+        <div class="app-content" :class="{'dark-mode': darkMode}">
+            <div id="list-title-wrapper">
+                <h5 :class="{'active-component': isToDo}" @click="componentChangeTo('to-do-list')">To Do List</h5>
+                <h5 :class="{'active-component': isNotToDo}" @click="componentChangeTo('not-to-do-list')">Not To Do List</h5>
+                <h5 :class="{'active-component': isPreferences}" @click="componentChangeTo('preferences-setting');">
+                    <font-awesome-icon icon="fa-solid fa-gear"/>
+                </h5>
+            </div>
+            <!-- keep-aliveはなくても動くが切り替えの描画に影響する -->
+            <keep-alive>
+                <component :is="currentComponent" :darkMode=darkMode @click="toggleScreenMode"></component>
+            </keep-alive>
         </div>
-        <!-- keep-aliveはなくても動くが切り替えの描画に影響する -->
-        <keep-alive>
-            <component :is="currentComponent"></component>
-        </keep-alive>
     </div>
 </template>
 
 <script>
 import ToDoList from './components/ToDoList.vue'
 import NotToDoList from './components/NotToDoList.vue'
+import Preferences from './components/Preferences.vue'
 
 export default {
     name: 'App',
     data() {
         return {
-            currentComponent: 'to-do-list',
+            darkMode: false,
             isToDo: true,
             isNotToDo: false,
+            isPreferences: false,
+            currentComponent: 'to-do-list',
         }
     },
     components: {
         'to-do-list': ToDoList,
         'not-to-do-list': NotToDoList,
+        'preferences-setting': Preferences,
+    },
+    created() {
+        chrome.storage.local.get('darkMode', function(item) {
+            if (typeOf(item.darkMode) === 'undefined') {
+                this.darkMode = false;
+                return;
+            }
+            this.darkMode = item.darkMode ? true : false;
+        }.bind(this));
     },
     methods: {
         componentChangeTo(componentName) {
@@ -34,10 +52,20 @@ export default {
             if (this.currentComponent === 'to-do-list') {
                 this.isToDo = true;
                 this.isNotToDo = false;
-            } else {
+                this.isPreferences = false;
+            } else if (this.currentComponent === 'not-to-do-list') {
                 this.isToDo = false;
                 this.isNotToDo = true;
+                this.isPreferences = false;
+            } else {
+                this.isToDo = false;
+                this.isNotToDo = false;
+                this.isPreferences = true;
             }
+        },
+        toggleScreenMode(value) {
+            this.darkMode = value;
+            this.setToChromeStorage('darkMode', this.darkMode);
         },
     },
 }
---
+++ src/components/Preferences.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="custom-control custom-switch">
+        <input type="checkbox" class="custom-control-input" id="customSwitch1" @change="turnOverScreen">
+            <label class="custom-control-label" for="customSwitch1">
+                Dark Mode
+            </label>
+        </div>
+    </div>
+</template>
+
+<script>
+import mixinFunctions from "../mixinFunctions";
+
+export default {
+    mixins: [mixinFunctions],
+    props: {
+        darkMode: {
+            type: Boolean,
+            required: true,
+        },
+    },
+    methods:{
+        turnOverScreen() {
+            this.$emit("click", !this.darkMode);
+        },
+    },
+}
+</script>
