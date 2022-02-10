c403aff524e8d3ccdce0649faee665fa27af8de8
こた <tokyosato1@gmail.com>
2022-02-10 15:19:11 +0900

    npm fontawesome install and setting

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// FontAwesome start
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faGear } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faGear);
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+// FontAwesome end
+
 require('./assets/scss/style.scss')
 require('./assets/scss/dark-mode.scss')
 
