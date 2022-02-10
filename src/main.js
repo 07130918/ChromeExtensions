4057abedf74e95d3163821fea7f674ab6fb1e009
こた <tokyosato1@gmail.com>
2022-02-10 12:48:40 +0900

    デザイン変更

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 require('./assets/scss/style.scss')
+require('./assets/scss/dark-mode.scss')
 
 Vue.config.productionTip = false
 
