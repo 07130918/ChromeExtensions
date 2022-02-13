84a58b4ab8d82181e9fc42633262028670038c9c
こた <tokyosato1@gmail.com>
2022-02-13 11:28:11 +0900

    :recycle: リファクタリング

--- extensiton_build.py
+++ extensiton_build.py
@@ -1,29 +1,42 @@
-# make manifest.json and set icon to dist
-
+# make manifest.json and set icon to dist/icons
 import os
 import json
 import shutil
 
 
 def main():
+    os.system('npm run build')
+    write_manifest()
+    set_icons()
+
+
+def write_manifest():
     content = {
         "name": "To Do List",
-        "description": "A simple to-do list extension, You can also write your own not to do list",
+        "description": "A simple to-do list extension, You can also write your own not-to-do list",
         "manifest_version": 3,
         "version": "1.0",
         "permissions": ["activeTab", "scripting", "storage"],
+        "icons": {
+            "16": "icons/icon16.png",
+            "48": "icons/icon48.png",
+            "128": "icons/icon128.png",
+        },
         "action": {
-            "default_icon": "icon.png",
+            "default_icon": "icons/icon.png",
             "default_popup": "index.html"
         }
     }
 
-    os.system('npm run build')
-    os.system('touch dist/manifest.json')
-    with open('dist/manifest.json', 'w') as f:
+    os.system('touch ./dist/manifest.json')
+    with open('./dist/manifest.json', 'w') as f:
         json.dump(content, f, ensure_ascii=False, indent=4, separators=(',', ': '))
 
-    shutil.copy('./icon.png', './dist')
+
+def set_icons():
+    os.system('mkdir dist/icons')
+    for icon in ['icon.png', 'icon16.png', 'icon48.png', 'icon128.png']:
+        shutil.copy(f'./src/assets/icons/{icon}', './dist/icons/')
 
 
 if __name__ == '__main__':
