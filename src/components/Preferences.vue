75e8e0c3437b74dcbac2b6b52adeae2bb34ffe47
こた <tokyosato1@gmail.com>
2022-02-13 12:27:00 +0900

    :zap: リンク追加

--- src/components/Preferences.vue
+++ src/components/Preferences.vue
@@ -1,23 +1,29 @@
 <template>
     <div>
-        <div class="custom-control custom-switch btn-dark-mode">
-        <input type="checkbox" class="custom-control-input" id="customSwitch1" @change="toggleDarkMode" :checked="darkMode">
-            <label class="custom-control-label" for="customSwitch1">
-                Dark Mode
-            </label>
-        </div>
-        <button class="btn btn-outline-danger btn-sm reset-all" @click="resetBothLists">Reset All</button>
-        <footer>
-            <a class="site-link" href="https://word-pop-quiz.netlify.app/" target="_blank" rel="noopener noreferrer">
-                <font-awesome-icon icon="fa-solid fa-link" />
-                単語クイズに挑戦
-            </a>
-            <div class="exception-clause">
-                ※ リストは随時自動保存されます。
-                <br>
-                ※ データ保存は必ずしも保証されません。
+        <div class="preferences">
+            <div class="custom-control custom-switch btn-dark-mode">
+            <input type="checkbox" class="custom-control-input" id="customSwitch1" @change="toggleDarkMode" :checked="darkMode">
+                <label class="custom-control-label" for="customSwitch1">
+                    Dark Mode
+                </label>
             </div>
-        </footer>
+            <button class="btn btn-outline-danger btn-sm reset-all" @click="resetBothLists">Reset All</button>
+            <footer>
+                <a class="site-link" href="https://word-pop-quiz.netlify.app/" target="_blank" rel="noopener noreferrer">
+                    <font-awesome-icon icon="fa-solid fa-link" />
+                    単語クイズに挑戦
+                </a>
+                <a class="site-link sm" href="https://github.com/07130918/ChromeExtentions/issues" target="_blank" rel="noopener noreferrer">
+                    <font-awesome-icon icon="fa-solid fa-comments" />
+                    この拡張機能のバグを報告/要望を投稿
+                </a>
+                <div class="exception-clause">
+                    ※ リストは随時自動保存されます。
+                    <br>
+                    ※ データ保存は必ずしも保証されません。
+                </div>
+            </footer>
+        </div>
     </div>
 </template>
 
